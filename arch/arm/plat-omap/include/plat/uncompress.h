c3d1a3ca4fdbbee88f5b01faa674ce10f660ad2d
Colin Cross <ccross@android.com>
2011-05-19 11:30:58 -0700

    ARM: omap4: tuna: Move debug uart to uart3 for lunchbox
    Change-Id: I7bc3ae03260ea0a00fcd30667cfb897abf9fc6a5
    Signed-off-by: Colin Cross <ccross@android.com>

--- arch/arm/plat-omap/include/plat/uncompress.h
+++ arch/arm/plat-omap/include/plat/uncompress.h
@@ -165,10 +165,10 @@ static inline void __arch_decomp_setup(unsigned long arch_id)
 		DEBUG_LL_OMAP4(3, omap_4430sdp);
 		DEBUG_LL_OMAP4(3, omap4_panda);
 		DEBUG_LL_OMAP4(3, omap_tabletblaze);
+		DEBUG_LL_OMAP4(3, tuna);
 
 		/* omap4 based boards using UART4 */
 		DEBUG_LL_OMAP4(4, omap4_espresso)
-		DEBUG_LL_OMAP4(4, tuna);
 
 		/* zoom2/3 external uart */
 		DEBUG_LL_ZOOM(omap_zoom2);
