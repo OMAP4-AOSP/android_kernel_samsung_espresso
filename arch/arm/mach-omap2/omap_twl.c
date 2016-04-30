aecc333d9920cfc5fb686843530639c0372f6ea1
Ziyan <jaraidaniel@gmail.com>
2016-04-30 20:32:34 +0200

    mach-omap2: omap_twl: ifdef register changes for espresso

--- arch/arm/mach-omap2/omap_twl.c
+++ arch/arm/mach-omap2/omap_twl.c
@@ -315,8 +315,13 @@ static struct omap_voltdm_pmic omap443x_446x_iva_pmic = {
 	.max_volt		= 1418000,
 	.vp_timeout_us		= OMAP4_VP_VLIMITTO_TIMEOUT_US,
 	.i2c_slave_addr		= TWL6030_SRI2C_SLAVE_ADDR,
+#ifdef CONFIG_MACH_OMAP4_ESPRESSO
+	.volt_reg_addr		= TWL6032_SMPS5_SR_VOLT_REG;
+	.cmd_reg_addr		= TWL6032_SMPS5_SR_CMD_REG;
+#else
 	.volt_reg_addr		= TWL6030_VCORE2_SR_VOLT_REG,
 	.cmd_reg_addr		= TWL6030_VCORE2_SR_CMD_REG,
+#endif
 	.i2c_high_speed		= true,
 	.i2c_scll_low		= 0x28,
 	.i2c_scll_high		= 0x2C,
@@ -369,8 +374,13 @@ static struct omap_voltdm_pmic omap443x_core_pmic = {
 	.max_volt		= 1418000,
 	.vp_timeout_us		= OMAP4_VP_VLIMITTO_TIMEOUT_US,
 	.i2c_slave_addr		= TWL6030_SRI2C_SLAVE_ADDR,
+#ifdef CONFIG_MACH_OMAP4_ESPRESSO
+	.volt_reg_addr		= TWL6032_SMPS2_SR_VOLT_REG;
+	.cmd_reg_addr		= TWL6032_SMPS2_SR_CMD_REG;
+#else
 	.volt_reg_addr		= TWL6030_VCORE3_SR_VOLT_REG,
 	.cmd_reg_addr		= TWL6030_VCORE3_SR_CMD_REG,
+#endif
 	.i2c_high_speed		= true,
 	.i2c_scll_low		= 0x28,
 	.i2c_scll_high		= 0x2C,
@@ -672,20 +682,6 @@ int __init omap_twl_init(void)
 
 	if (cpu_is_omap44xx())
 		desc = &twl6030_pmic_desc;
-
-	if (cpu_is_omap443x() && is_twl6030_lite()) {
-		omap443x_core_pmic.volt_reg_addr
-			= TWL6032_SMPS2_SR_VOLT_REG;
-		omap443x_core_pmic.cmd_reg_addr
-			= TWL6032_SMPS2_SR_CMD_REG;
-
-		omap443x_446x_iva_pmic.volt_reg_addr
-			= TWL6032_SMPS5_SR_VOLT_REG;
-		omap443x_446x_iva_pmic.cmd_reg_addr
-			= TWL6032_SMPS5_SR_CMD_REG;
-		pr_info("%s: TWL6032 PMIC SR Addr Change Success!!\n",
-				__func__);
-	}
 
 	return omap_pmic_register_data(omap_twl_map, desc);
 }
