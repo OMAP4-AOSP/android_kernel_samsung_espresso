21d0aec4ef2a529ac0d764667b90381c4b724969
Ziyan <jaraidaniel@gmail.com>
2016-05-04 23:13:05 +0200

    [espresso] mmc: disable MMC_CAP_ERASE for espresso

--- drivers/mmc/host/omap_hsmmc.c
+++ drivers/mmc/host/omap_hsmmc.c
@@ -2629,7 +2629,11 @@ static int __devinit omap_hsmmc_probe(struct platform_device *pdev)
 	mmc->max_seg_size = mmc->max_req_size;
 
 	mmc->caps |= MMC_CAP_MMC_HIGHSPEED | MMC_CAP_SD_HIGHSPEED |
-		     MMC_CAP_WAIT_WHILE_BUSY | MMC_CAP_ERASE;
+#ifdef CONFIG_MACH_OMAP4_ESPRESSO
+		     MMC_CAP_WAIT_WHILE_BUSY;
+#else
+                     MMC_CAP_WAIT_WHILE_BUSY | MMC_CAP_ERASE;
+#endif
 
 	mmc->caps |= mmc_slot(host).caps;
 	if (mmc->caps & MMC_CAP_8_BIT_DATA)
