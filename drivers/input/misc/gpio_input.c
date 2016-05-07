f85ab2fe7dc400fe4e965c1a52bf483784a9ba56
Ziyan <jaraidaniel@gmail.com>
2016-05-07 22:21:45 +0200

    [REVERT ME] Reboot on key press to allow debugging by last_kmsg
    Conflicts:
    	drivers/input/misc/gpio_input.c

--- drivers/input/misc/gpio_input.c
+++ drivers/input/misc/gpio_input.c
@@ -21,6 +21,8 @@
 #include <linux/interrupt.h>
 #include <linux/slab.h>
 #include <linux/pm_wakeup.h>
+#include <linux/reboot.h>
+#include "../../../arch/arm/mach-omap2/board-espresso.h"
 
 enum {
 	DEBOUNCE_UNSTABLE     = BIT(0),	/* Got irq, while debouncing */
@@ -132,6 +134,9 @@ static enum hrtimer_restart gpio_event_input_timer_func(struct hrtimer *timer)
 				key_entry->code, i, key_entry->gpio, pressed);
 		input_event(ds->input_devs->dev[key_entry->dev], ds->info->type,
 			    key_entry->code, pressed);
+		if (pressed == 1) {
+			espresso_restart();
+		}
 		sync_needed = true;
 	}
 	if (sync_needed) {
