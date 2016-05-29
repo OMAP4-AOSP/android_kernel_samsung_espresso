a027adacedd54704d3f59ac95ba87a575c8949b3
Ziyan <jaraidaniel@gmail.com>
2016-05-29 13:27:02 +0200

    espresso: power: switch to upstream smb347 driver

--- arch/arm/mach-omap2/board-espresso-power.c
+++ arch/arm/mach-omap2/board-espresso-power.c
@@ -26,6 +26,7 @@
 #include <linux/i2c-gpio.h>
 #include <linux/i2c/twl.h>
 #include <linux/power/max17042_battery.h>
+#include <linux/power/smb347-charger.h>
 #include <linux/battery.h>
 #include <linux/irq.h>
 
@@ -50,17 +51,6 @@
 #define LOW_BLOCK_TEMP		(-50)
 #define LOW_RECOVER_TEMP	0
 
-static irqreturn_t charger_state_isr(int irq, void *_data)
-{
-	int val;
-
-	val = gpio_get_value(GPIO_TA_NCHG);
-
-	irq_set_irq_type(irq, val ? IRQF_TRIGGER_LOW : IRQF_TRIGGER_HIGH);
-
-	return IRQ_HANDLED;
-}
-
 static irqreturn_t fuel_alert_isr(int irq, void *_data)
 {
 	int val;
@@ -73,19 +63,9 @@ static irqreturn_t fuel_alert_isr(int irq, void *_data)
 
 static void charger_gpio_init(void)
 {
-	int irq, fuel_irq;
+	int fuel_irq;
 	int ret;
 	struct gpio charger_gpios[] = {
-		{
-			.flags = GPIOF_IN,
-			.gpio  = GPIO_TA_NCHG,
-			.label = "TA_nCHG"
-		},
-		{
-			.flags = GPIOF_OUT_INIT_LOW,
-			.gpio  = GPIO_TA_EN,
-			.label = "TA_EN"
-		},
 		{
 			.flags = GPIOF_IN,
 			.gpio  = GPIO_FUEL_ALERT,
@@ -95,14 +75,6 @@ static void charger_gpio_init(void)
 
 	gpio_request_array(charger_gpios, ARRAY_SIZE(charger_gpios));
 
-	irq = gpio_to_irq(GPIO_TA_NCHG);
-	ret = request_threaded_irq(irq, NULL, charger_state_isr,
-			IRQF_TRIGGER_LOW | IRQF_ONESHOT | IRQF_NO_SUSPEND,
-			"Charge_Status", NULL);
-	if (unlikely(ret < 0))
-		pr_err("%s: request irq %d failed for gpio %d\n",
-			__func__, irq, GPIO_TA_NCHG);
-
 	fuel_irq = gpio_to_irq(GPIO_FUEL_ALERT);
 	ret = request_threaded_irq(fuel_irq, NULL, fuel_alert_isr,
 			IRQF_TRIGGER_FALLING,
@@ -110,13 +82,6 @@ static void charger_gpio_init(void)
 	if (unlikely(ret < 0))
 		pr_err("%s: request fuel alert irq %d failed for gpio %d\n",
 			__func__, fuel_irq, GPIO_FUEL_ALERT);
-}
-
-static void charger_enble_set(int state)
-{
-	gpio_set_value(GPIO_TA_EN, !state);
-	pr_debug("%s: Set charge status: %d, current status: %d\n",
-		__func__, state, !state);
 }
 
 static struct i2c_gpio_platform_data espresso_gpio_i2c5_pdata = {
@@ -149,21 +114,50 @@ static struct platform_device espresso_gpio_i2c7_device = {
 	},
 };
 
-static void set_chg_state(int cable_type)
-{
-	omap4_espresso_usb_detected(cable_type);
-	omap4_espresso_tsp_ta_detect(cable_type);
-}
+static char *espresso_battery[] = {
+	"max17042_battery",
+};
+
+static struct smb347_charger_platform_data smb347_charger_pdata = {
+	.battery_info = {
+		.technology		= POWER_SUPPLY_TECHNOLOGY_LION,
+		.voltage_max_design	= 4220000,
+		.voltage_min_design	= 2500000,
+	},
+	.max_charge_current	= 2200000,
+	.max_charge_voltage	= 4200000,
+	.pre_charge_current	= 250000,
+	.termination_current	= 250000,
+	.mains_current_limit	= 1800000,
+	.usb_hc_current_limit	= 1800000,
+	.soft_cold_temp_limit	= SMB347_TEMP_USE_DEFAULT,
+	.soft_hot_temp_limit	= 42,
+	.hard_cold_temp_limit	= SMB347_TEMP_USE_DEFAULT,
+	.hard_hot_temp_limit	= 50,
+	.suspend_on_hard_temp_limit = true,
+	.use_mains		= true,
+	.use_usb		= true,
+	.use_usb_otg		= false,
+	.disable_automatic_recharge = false,
+	.irq_gpio		= GPIO_TA_NCHG,
+	.disable_stat_interrupts = true,
+	.enable_control		= SMB347_CHG_ENABLE_PIN_ACTIVE_LOW,
+	.usb_mode_pin_ctrl	= false,
+	.supplied_to		= espresso_battery,
+	.num_supplicants	= ARRAY_SIZE(espresso_battery),
+	.en_gpio		= GPIO_TA_EN,
+};
 
 static const __initdata struct i2c_board_info smb136_i2c[] = {
 	{
-		I2C_BOARD_INFO("smb136-charger", 0x4D), /* 9A >> 1 */
+		I2C_BOARD_INFO("smb136", 0x4D), /* 9A >> 1 */
 	},
 };
 
 static const __initdata struct i2c_board_info smb347_i2c[] = {
 	{
-		I2C_BOARD_INFO("smb347-charger", 0x0C >> 1),
+		I2C_BOARD_INFO("smb347", 0x0C >> 1),
+		.platform_data = &smb347_charger_pdata,
 	},
 };
 
@@ -184,6 +178,9 @@ int check_charger_type(void)
 {
 	int cable_type;
 	short adc;
+
+	if (gpio_is_valid(GPIO_TA_NCONNECTED) && gpio_get_value(GPIO_TA_NCONNECTED))
+		return CABLE_TYPE_NONE;
 
 	adc = omap4_espresso_get_adc(ADC_CHECK_1);
 	cable_type = adc > CABLE_DETECT_VALUE ?
@@ -215,18 +212,10 @@ void __init omap4_espresso_charger_init(void)
 	if (ret < 0)
 		pr_err("%s: gpio_i2c7 device register fail\n", __func__);
 
-#ifdef CONFIG_BATTERY_MANAGER
 	if (board_is_espresso10())
 		i2c_register_board_info(5, smb347_i2c, ARRAY_SIZE(smb347_i2c));
 	else
 		i2c_register_board_info(5, smb136_i2c, ARRAY_SIZE(smb136_i2c));
-#endif
 
 	i2c_register_board_info(7, max17042_i2c, ARRAY_SIZE(max17042_i2c));
-
-#ifdef CONFIG_BATTERY_MANAGER
-	ret = platform_device_register(&battery_manager_device);
-	if (ret < 0)
-		pr_err("%s: battery monitor device register fail\n", __func__);
-#endif
 }
