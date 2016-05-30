772d64f01198010f0aaf94a8b30ba89e96dd34ee
Ziyan <jaraidaniel@gmail.com>
2016-05-30 13:35:13 +0200

    espresso: power: move charger management to espresso-power
    Also enabled stat interrupts, and simplified the charger management.
    Since SMB347 manages itself (ie. automatically stops charging on overheat),
    this is sufficient. Also fixes USB charging.

--- arch/arm/mach-omap2/board-espresso.h
+++ arch/arm/mach-omap2/board-espresso.h
@@ -83,6 +83,7 @@ void bcm_bt_lpm_exit_lpm_locked(struct uart_port *uport);
 
 /** @category charger */
 void omap4_espresso_charger_init(void);
+void omap4_espresso_update_charger(int cable_type);
 
 /** @category modem*/
 void omap4_espresso_modem_init(void);
