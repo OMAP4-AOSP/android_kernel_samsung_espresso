CyanogenMod/android_kernel_samsung_espresso10

serio: define Samsung serio protocol

include/linux/serio.h
@@ -199,5 +199,6 @@ static inline void serio_continue_rx(struct serio *serio)
 #define SERIO_DYNAPRO	0x3a
 #define SERIO_HAMPSHIRE	0x3b
 #define SERIO_PS2MULT	0x3c
+#define SERIO_SAMSUNG	0x3d
 
 #endif

OMAP4-AOSP/android_kernel_samsung_espresso

include/linux/serio.h
@@ -202,5 +202,6 @@ static inline void serio_continue_rx(struct serio *serio)
 #define SERIO_HAMPSHIRE	0x3b
 #define SERIO_PS2MULT	0x3c
 #define SERIO_TSC40	0x3d
+#define SERIO_SAMSUNG	0x3e
 
 #endif