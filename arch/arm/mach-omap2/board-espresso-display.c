bdbaee0f73fd84cfe36ae6dd40d6798c44d5fa18
Ziyan <jaraidaniel@gmail.com>
2016-07-16 15:25:24 +0200

    ARM: OMAP4: add Espresso board files from kernel 3.4

---
+++ arch/arm/mach-omap2/board-espresso-display.c
@@ -0,0 +1,160 @@
+/* arch/arm/mach-omap2/board-espresso-display.c
+ *
+ * Copyright (C) 2011 Samsung Electronics Co, Ltd.
+ *
+ * Based on mach-omap2/board-tuna-display.c
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/err.h>
+#include <linux/gpio.h>
+#include <linux/kernel.h>
+#include <linux/omapfb.h>
+#include <linux/platform_data/panel-ltn.h>
+
+#include <plat/omap_hwmod.h>
+#include <plat/android-display.h>
+
+#include <video/omapdss.h>
+
+#include "board-espresso.h"
+
+#define GPIO_LED_BACKLIGHT_RESET	95
+#define GPIO_LCD_EN			135
+#define GPIO_LVDS_NSHDN		136
+
+static int espresso_lcd_enable(struct omap_dss_device *dssdev)
+{
+	pr_debug("%s\n", __func__);
+	gpio_set_value(GPIO_LCD_EN, 1);
+
+	return 0;
+}
+
+static void espresso_lcd_disable(struct omap_dss_device *dssdev)
+{
+	pr_debug("%s\n", __func__);
+	gpio_set_value(GPIO_LCD_EN, 0);
+}
+
+static struct ltn_panel_data espresso_panel_data = {
+	.lvds_nshdn_gpio		= GPIO_LVDS_NSHDN,
+	.led_backlight_reset_gpio	= GPIO_LED_BACKLIGHT_RESET,
+	.backlight_gptimer_num		= 10,
+	.brightness_table = {
+		.platform_value	= {BRIGHTNESS_OFF, BRIGHTNESS_DIM, BRIGHTNESS_MIN,
+				BRIGHTNESS_25, BRIGHTNESS_DEFAULT, BRIGHTNESS_MAX},
+		.kernel_value	= { 0, 2, 3, 8, 40, 85 },
+	},
+};
+
+static struct omap_dss_device espresso_lcd_device = {
+	.name			= "lcd",
+	.driver_name		= "ltn_panel",
+	.type			= OMAP_DISPLAY_TYPE_DPI,
+	.phy.dpi.data_lines	= 24,
+	.data			= &espresso_panel_data,
+	.channel		= OMAP_DSS_CHANNEL_LCD2,
+	.platform_enable	= espresso_lcd_enable,
+	.platform_disable	= espresso_lcd_disable,
+	.panel = {
+		.timings	= {
+			.x_res		= 1024,
+			.y_res		= 600,
+			.pixel_clock	= 56888, /* closest to 56000 */
+			.hfp		= 186,
+			.hsw		= 50,
+			.hbp		= 210,
+			.vfp		= 24,
+			.vsw		= 10,
+			.vbp		= 11,
+		},
+		.width_in_um	= 153600,
+		.height_in_um	= 90000,
+	},
+};
+
+static struct omap_dss_device espresso10_lcd_config = {
+	.panel = {
+		.timings	= {
+			.x_res		= 1280,
+			.y_res		= 800,
+			.pixel_clock	= 69818, /* closest to 69000 */
+			.hfp		= 16,
+			.hsw		= 48,
+			.hbp		= 64,
+			.vfp		= 16,
+			.vsw		= 3,
+			.vbp		= 11,
+		},
+		.width_in_um	= 216960,
+		.height_in_um	= 135600,
+	},
+};
+
+static struct omap_dss_device *espresso_dss_devices[] = {
+	&espresso_lcd_device,
+};
+
+static struct omap_dss_board_info espresso_dss_data = {
+	.num_devices	= ARRAY_SIZE(espresso_dss_devices),
+	.devices	= espresso_dss_devices,
+	.default_device	= &espresso_lcd_device,
+};
+
+static struct sgx_omaplfb_config espresso_omaplfb_config[] = {
+	{
+		.tiler2d_buffers = 2,
+		.swap_chain_length = 2,
+	},
+};
+
+static struct sgx_omaplfb_platform_data espresso_omaplfb_plat_data = {
+	.num_configs = ARRAY_SIZE(espresso_omaplfb_config),
+	.configs = espresso_omaplfb_config,
+};
+
+static struct omapfb_platform_data espresso_fb_pdata = {
+	.mem_desc = {
+		.region_cnt = ARRAY_SIZE(espresso_omaplfb_config),
+	},
+};
+
+void __init omap4_espresso_memory_display_init(void)
+{
+	if (board_is_espresso10())
+		espresso_dss_data.devices[0]->panel = espresso10_lcd_config.panel;
+
+	omap_android_display_setup(&espresso_dss_data,
+				   NULL,
+				   &espresso_omaplfb_plat_data,
+				   &espresso_fb_pdata);
+}
+
+void __init omap4_espresso_display_init(void)
+{
+	int ret;
+
+	if (board_is_espresso10())
+		espresso_panel_data.pwm_duty_max = 1600; /* 25kHz */
+	else
+		espresso_panel_data.pwm_duty_max = 1200; /* 32kHz */
+
+	ret = gpio_request(GPIO_LCD_EN, "lcd_en");
+	if (ret < 0)
+		pr_err("%s: gpio_request %d failed!\n", __func__, GPIO_LCD_EN);
+
+	gpio_direction_output(GPIO_LCD_EN, 1);
+
+	omapfb_set_platform_data(&espresso_fb_pdata);
+
+	omap_display_init(&espresso_dss_data);
+}
---
+++ arch/arm/mach-omap2/board-espresso.c
@@ -0,0 +1,361 @@
+/* arch/arm/mach-omap2/board-espresso.c
+ *
+ * Copyright (C) 2011 Samsung Electronics Co, Ltd.
+ *
+ * Based on mach-omap2/board-espresso.c
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/input.h>
+#include <linux/err.h>
+#include <linux/gpio.h>
+#include <linux/ion.h>
+#include <linux/memblock.h>
+#include <linux/omap_ion.h>
+#include <linux/reboot.h>
+#include <linux/sysfs.h>
+
+#include <plat/board.h>
+#include <plat/common.h>
+#include <plat/cpu.h>
+#include <plat/drm.h>
+#include <plat/omap_apps_brd_id.h>
+#include <plat/remoteproc.h>
+#include <plat/usb.h>
+
+#include <mach/id.h>
+
+#include <asm/hardware/gic.h>
+
+#include <asm/mach-types.h>
+#include <asm/mach/arch.h>
+
+#include <asm/system_info.h>
+#include <asm/system_misc.h>
+
+#include "mach/omap-secure.h"
+
+#include "board-espresso.h"
+#include "common.h"
+#include "control.h"
+#include "mux.h"
+#include "omap_ram_console.h"
+#include "omap4_ion.h"
+#include "omap4-sar-layout.h"
+
+#include "sec_muxtbl.h"
+
+/* gpio to distinguish WiFi and USA-BBY (P51xx)
+ *
+ * HW_REV4 | HIGH | LOW
+ * --------+------+------
+ *         |IrDA O|IrDA X
+ */
+#define GPIO_HW_REV4		41
+
+#define GPIO_TA_NCONNECTED	32
+
+#define ESPRESSO_MEM_BANK_0_SIZE	0x20000000
+#define ESPRESSO_MEM_BANK_0_ADDR	0x80000000
+#define ESPRESSO_MEM_BANK_1_SIZE	0x20000000
+#define ESPRESSO_MEM_BANK_1_ADDR	0xA0000000
+
+#define OMAP_SW_BOOT_CFG_ADDR	0x4A326FF8
+#define REBOOT_FLAG_NORMAL	(1 << 0)
+#define REBOOT_FLAG_RECOVERY	(1 << 1)
+#define REBOOT_FLAG_POWER_OFF	(1 << 4)
+#define REBOOT_FLAG_DOWNLOAD	(1 << 5)
+
+#define ESPRESSO_ATTR_RO(_type, _name, _show) \
+	struct kobj_attribute espresso_##_type##_prop_attr_##_name = \
+		__ATTR(_name, S_IRUGO, _show, NULL)
+
+struct class *sec_class;
+EXPORT_SYMBOL(sec_class);
+
+static struct platform_device bcm4330_bluetooth_device = {
+	.name		= "bcm4330_bluetooth",
+	.id		= -1,
+};
+
+static struct platform_device *espresso_devices[] __initdata = {
+	&bcm4330_bluetooth_device,
+};
+
+static const struct usbhs_omap_board_data usbhs_bdata __initconst = {
+	.port_mode[0] = OMAP_USBHS_PORT_MODE_UNUSED,
+	.port_mode[1] = OMAP_USBHS_PORT_MODE_UNUSED,
+	.port_mode[2] = OMAP_USBHS_PORT_MODE_UNUSED,
+	.phy_reset  = false,
+	.reset_gpio_port[0]  = -EINVAL,
+	.reset_gpio_port[1]  = -EINVAL,
+	.reset_gpio_port[2]  = -EINVAL
+};
+
+static struct omap_musb_board_data musb_board_data = {
+	.interface_type	= MUSB_INTERFACE_UTMI,
+	.mode		= MUSB_OTG,
+	.power		= 500,
+};
+
+/* Board identification */
+
+static bool _board_has_modem = true;
+static bool _board_is_espresso10 = true;
+static bool _board_is_bestbuy_variant = false;
+
+/*
+ * Sets the board type
+ */
+static __init int setup_board_type(char *str)
+{
+	int lcd_id;
+
+	/* We reset the console loglevel here back to verbose, as our
+	 * bootloaders pass loglevel=0 to the kernel cmdline.
+	 * This is the most convinient place to do so, as this method
+	 * gets executed right after parsing the loglevel param.
+	 */
+	console_loglevel = 15;
+
+	if (kstrtoint(str, 0, &lcd_id)) {
+		pr_err("************************************************\n");
+		pr_err("Cannot parse lcd_panel_id command line parameter\n");
+		pr_err("Failed to detect board type, assuming espresso10\n");
+		pr_err("************************************************\n");
+		return 1;
+	}
+
+	/*
+	 * P51xx bootloaders pass lcd_id=1 and on some older lcd_id=0,
+	 * everything else is P31xx.
+	 */
+	if (lcd_id > 1)
+		_board_is_espresso10 = false;
+
+	return 0;
+}
+early_param("lcd_panel_id", setup_board_type);
+
+/*
+ * Sets whether the device is a wifi-only variant
+ */
+static int __init espresso_set_subtype(char *str)
+{
+	#define CARRIER_WIFI_ONLY "wifi-only"
+
+	if (!strncmp(str, CARRIER_WIFI_ONLY, strlen(CARRIER_WIFI_ONLY)))
+		_board_has_modem = false;
+
+	return 0;
+}
+__setup("androidboot.carrier=", espresso_set_subtype);
+
+/*
+ * Sets whether the device is a Best Buy wifi-only variant
+ */
+static int __init espresso_set_vendor_type(char *str)
+{
+	unsigned int vendor;
+
+	if (kstrtouint(str, 0, &vendor))
+		return 0;
+
+	if (vendor == 0)
+		_board_is_bestbuy_variant = true;
+
+	return 0;
+}
+__setup("sec_vendor=", espresso_set_vendor_type);
+
+bool board_is_espresso10(void) {
+	return _board_is_espresso10;
+}
+
+bool board_has_modem(void) {
+	return _board_has_modem;
+}
+
+bool board_is_bestbuy_variant(void) {
+	return _board_is_bestbuy_variant;
+}
+
+/* Board identification end */
+
+static void espresso_power_off_charger(void)
+{
+	pr_err("Rebooting into bootloader for charger.\n");
+	arm_pm_restart('t', NULL);
+}
+
+static int espresso_reboot_call(struct notifier_block *this,
+				unsigned long code, void *cmd)
+{
+	u32 flag = REBOOT_FLAG_NORMAL;
+	char *blcmd = "RESET";
+
+	if (code == SYS_POWER_OFF) {
+		flag = REBOOT_FLAG_POWER_OFF;
+		blcmd = "POFF";
+		if (!gpio_get_value(GPIO_TA_NCONNECTED))
+			pm_power_off = espresso_power_off_charger;
+	} else if (code == SYS_RESTART) {
+		if (cmd) {
+			if (!strcmp(cmd, "recovery"))
+				flag = REBOOT_FLAG_RECOVERY;
+			else if (!strcmp(cmd, "download"))
+				flag = REBOOT_FLAG_DOWNLOAD;
+		}
+	}
+
+	omap_writel(flag, OMAP_SW_BOOT_CFG_ADDR);
+	omap_writel(*(u32 *) blcmd, OMAP_SW_BOOT_CFG_ADDR - 0x04);
+
+	return NOTIFY_DONE;
+}
+
+static struct notifier_block espresso_reboot_notifier = {
+	.notifier_call = espresso_reboot_call,
+};
+
+static void __init espresso10_update_board_type(void)
+{
+	/* because omap4_mux_init is not called when this function is
+	 * called, padconf reg must be configured by low-level function. */
+	omap_writew(OMAP_MUX_MODE3 | OMAP_PIN_INPUT,
+		    OMAP4_CTRL_MODULE_PAD_CORE_MUX_PBASE +
+		    OMAP4_CTRL_MODULE_PAD_GPMC_A17_OFFSET);
+
+	gpio_request(GPIO_HW_REV4, "HW_REV4");
+	if (gpio_get_value(GPIO_HW_REV4))
+		_board_is_bestbuy_variant = true;
+}
+
+#define ESPRESSO_ATTR_RO(_type, _name, _show) \
+	struct kobj_attribute espresso_##_type##_prop_attr_##_name = \
+		__ATTR(_name, S_IRUGO, _show, NULL)
+
+static ssize_t espresso_board_type_show(struct kobject *kobj,
+	 struct kobj_attribute *attr, char *buf)
+{
+	return sprintf(buf, "espresso%s%s", board_is_espresso10() ? "10" : "",
+					board_has_modem() ? "" : "wifi");
+}
+
+static ESPRESSO_ATTR_RO(board, type, espresso_board_type_show);
+static struct attribute *espresso_board_prop_attrs[] = {
+	&espresso_board_prop_attr_type.attr,
+	NULL,
+};
+
+static struct attribute_group espresso_board_prop_attr_group = {
+	.attrs = espresso_board_prop_attrs,
+};
+
+static void __init omap4_espresso_create_board_props(void)
+{
+	struct kobject *board_kobj;
+	int ret = 0;
+
+	board_kobj = kobject_create_and_add("board", NULL);
+	if (!board_kobj)
+		goto err_board_obj;
+
+	ret = sysfs_create_group(board_kobj, &espresso_board_prop_attr_group);
+	if (ret)
+		goto err_board_sysfs_create;
+
+	return;
+
+err_board_sysfs_create:
+	kobject_put(board_kobj);
+err_board_obj:
+	if (!board_kobj || ret)
+		pr_err("failed to create espresso board properties\n");
+}
+
+static void __init sec_common_init(void)
+{
+	sec_class = class_create(THIS_MODULE, "sec");
+	if (IS_ERR(sec_class))
+		pr_err("Failed to create class (sec)!\n");
+}
+
+static void __init espresso_init(void)
+{
+	omap4_mux_init(NULL, NULL, OMAP_PACKAGE_CBS);
+
+	omap4_espresso_emif_init();
+
+	if (board_is_espresso10()) {
+		espresso10_update_board_type();
+		if (board_is_bestbuy_variant() && system_rev >= 7)
+			sec_muxtbl_init(SEC_MACHINE_ESPRESSO10_USA_BBY, system_rev);
+		sec_muxtbl_init(SEC_MACHINE_ESPRESSO10, system_rev);
+	} else
+		sec_muxtbl_init(SEC_MACHINE_ESPRESSO, system_rev);
+
+	register_reboot_notifier(&espresso_reboot_notifier);
+
+	/* initialize sec common infrastructures */
+	sec_common_init();
+
+	/* initialize board props */
+	omap_create_board_props();
+	omap4_espresso_create_board_props();
+
+	/* initialize each drivers */
+	omap4_espresso_serial_init();
+	omap4_espresso_pmic_init();
+	omap_sdrc_init(NULL, NULL);
+	omap4_espresso_charger_init();
+#ifdef CONFIG_ION_OMAP
+	omap4_register_ion();
+#endif
+	platform_add_devices(espresso_devices, ARRAY_SIZE(espresso_devices));
+	omap_init_dmm_tiler();
+	omap4_espresso_sdio_init();
+	usbhs_init(&usbhs_bdata);
+	usb_musb_init(&musb_board_data);
+	omap4_espresso_connector_init();
+	omap4_espresso_display_init();
+	omap4_espresso_input_init();
+	omap4_espresso_wifi_init();
+	omap4_espresso_sensors_init();
+	omap4_espresso_jack_init();
+	omap4_espresso_modem_init();
+}
+
+static void __init espresso_reserve(void)
+{
+	omap_ram_console_init(OMAP_RAM_CONSOLE_START_DEFAULT,
+			OMAP_RAM_CONSOLE_SIZE_DEFAULT);
+	omap_rproc_reserve_cma(RPROC_CMA_OMAP4);
+	omap4_espresso_memory_display_init();
+	omap4_ion_init();
+	omap4_secure_workspace_addr_default();
+	omap_reserve();
+}
+
+MACHINE_START(OMAP4_ESPRESSO, "OMAP4 Espresso board")
+	/* Maintainer: Daniel Jarai */
+	.atag_offset	= 0x100,
+	.reserve	= espresso_reserve,
+	.map_io		= omap4_map_io,
+	.init_early	= omap4430_init_early,
+	.init_irq	= gic_init_irq,
+	.handle_irq	= gic_handle_irq,
+	.init_machine	= espresso_init,
+	.timer		= &omap4_timer,
+	.restart	= omap_prcm_restart,
+MACHINE_END
